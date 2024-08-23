a1f100a5caf69137a516786a10d732c36ad336a2
Ramish <rohullahramish9@gmail.com>
2024-08-23 15:56:47 -0400

    Added Icons and Fixed Loader

--- src/components/elements/LoadingView.tsx
+++ src/components/elements/LoadingView.tsx
@@ -1,5 +1,11 @@
+import { AiOutlineLoading3Quarters } from "react-icons/ai";
+
 function LoadingView() {
-  return <div>Loading...</div>;
+  return (
+    <div className="flex-1 w-full min-h-screen text-primary flex items-center justify-center">
+      <AiOutlineLoading3Quarters className="text-2xl animate-spin" />
+    </div>
+  );
 }
 
 export default LoadingView;
--- src/pages/movies/index.tsx
+++ src/pages/movies/index.tsx
@@ -4,18 +4,6 @@ import MainLayout from "@/layouts/MainLayout";
 function Movies() {
   return (
     <MainLayout>
-      {/* <h1 className="text-primary">Heading 1</h1>
-      <h2 className="text-secondary">Heading 2</h2>
-      <h3 className="text-base">Heading 3</h3>
-      <h4 className="text-neutral">Heading 4</h4>
-      <h5 className="text-accent">Heading 5</h5>
-      <h6 className="">Heading 6</h6>
-
-      <p className="text-lg">Body (Large)</p>
-      <p className="text-md">Body (Regular)</p>
-      <p className="text-sm">Body (Small)</p>
-      <p className="text-xs">Body (Extra Small)</p> */}
-
       <MoviesContainer />
     </MainLayout>
   );
