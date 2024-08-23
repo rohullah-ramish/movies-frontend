9993bb8de1e1443111eef36180f4e97f2c3115a0
Ramish <rohullahramish9@gmail.com>
2024-08-23 10:10:00 -0400

    Configured Typography, spacing & fonts

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,7 +1,20 @@
 import { PropsWithChildren } from "react";
 
+import { Montserrat, Poppins } from "next/font/google";
+
+const montserrat = Montserrat({ variable: "--montserrat", subsets: ["latin"] });
+const poppins = Poppins({
+  variable: "--poppins",
+  weight: ["600", "700"],
+  subsets: ["latin"],
+});
+
 function MainLayout(props: PropsWithChildren) {
-  return <div>{props.children}</div>;
+  return (
+    <div className={`${montserrat.variable} ${poppins.variable}`}>
+      {props.children}
+    </div>
+  );
 }
 
 export default MainLayout;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,44 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        primary: "#2BD17E",
+        secondary: "#EB5757",
+        base: "#093545",
+        neutral: "#224957",
+        accent: "#092C39",
+      },
+      spacing: {
+        "1": "2px",
+        "2": "4px",
+        "3": "8px",
+        "4": "12px",
+        "5": "16px",
+        "6": "24px",
+        "7": "32px",
+        "8": "40px",
+        "9": "48px",
+        "10": "64px",
+        "11": "80px",
+        "12": "120px",
+        "13": "160px",
+      },
+      fontSize: {
+        caption: ["14px", { lineHeight: "16px" }],
+        xs: ["12px", { lineHeight: "16px" }],
+        sm: ["14px", { lineHeight: "24px" }],
+        md: ["16px", { lineHeight: "24px" }],
+        lg: ["20px", { lineHeight: "32px" }],
+        xl: ["16px", { lineHeight: "24px" }],
+        "2xl": ["20px", { lineHeight: "24px" }],
+        "3xl": ["24px", { lineHeight: "32px" }],
+        "4xl": ["32px", { lineHeight: "40px" }],
+        "5xl": ["48px", { lineHeight: "56px" }],
+        "6xl": ["64px", { lineHeight: "80px" }],
+      },
+      fontFamily: {
+        montserrat: "var(--montserrat)",
+        poppins: "var(--poppins)",
       },
     },
   },
